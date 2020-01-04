00c9cceb2b1e21af3c5ab1072a454a3b6268dffc
huangpeigui <13824400496@163.com>
2020-01-04 11:24:43 +0800

    fix:替代注解

--- gui-system/src/main/java/com/gui/GuiApplication.java
+++ gui-system/src/main/java/com/gui/GuiApplication.java
@@ -7,40 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-@EnableAutoConfiguration(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-})
 @EnableTransactionManagement
 @ServletComponentScan
 @MapperScan("com.gui.*.dao")
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 @EnableCaching
 public class GuiApplication {
     public static void main(String[] args) {
         SpringApplication.run(GuiApplication.class, args);
-        System.out.println("\n" +
-                "////////////////////////////////////////////////////////////////////\n" +
-                "//                          _ooOoo_                               //\n" +
-                "//                         o8888888o                              //\n" +
-                "//                         88\" . \"88                              //\n" +
-                "//                         (| ^_^ |)                              //\n" +
-                "//                         O\\  =  /O                              //\n" +
-                "//                      ____/`---'\\____                           //\n" +
-                "//                    .'  \\\\|     |//  `.                         //\n" +
-                "//                   /  \\\\|||  :  |||//  \\                        //\n" +
-                "//                  /  _||||| -:- |||||-  \\                       //\n" +
-                "//                  |   | \\\\\\  -  /// |   |                       //\n" +
-                "//                  | \\_|  ''\\---/''  |   |                       //\n" +
-                "//                  \\  .-\\__  `-`  ___/-. /                       //\n" +
-                "//                ___`. .'  /--.--\\  `. . ___                     //\n" +
-                "//              .\"\" '<  `.___\\_<|>_/___.'  >'\"\".                  //\n" +
-                "//            | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |                 //\n" +
-                "//            \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /                 //\n" +
-                "//      ========`-.____`-.___\\_____/___.-`____.-'========         //\n" +
-                "//                           `=---='                              //\n" +
-                "//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //\n" +
-                "//             佛祖保佑          永无BUG         永不修改         //\n" +
-                "////////////////////////////////////////////////////////////////////\n"
-               );
     }
 }
